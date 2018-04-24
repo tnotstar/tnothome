00e3aa634b503df9d9a333ea250e325145b7e832
Antonio Alvarado H <tnotstar@gmail.com>
2018-04-24 19:05:54 +0200

    added some dialogs to `shui.cmd`
    - created options for: users, disks, devices, and so on

--- bin/shui.cmd
+++ bin/shui.cmd
@@ -15,23 +15,73 @@
 @rem limitations under the License.
 @rem
 
-@if "%1" == "eject" goto :eject
+@if "%1" == "users" goto :users
+@if "%1" == "disks" goto :disks
+@if "%1" == "devices" goto :devices
 @if "%1" == "services" goto :services
+@if "%1" == "certificates" goto :certificates
+@if "%1" == "eject" goto :eject
+@if "%1" == "control"  goto :control
+@goto :help
 
 :help
 @echo Show some Windows' UI dialogs from the command line
 @echo.
-@echo     %~nx0 eject
-@echo     %~nx0 services
+@echo     %~n0 users
+@echo     %~n0 disks
+@echo     %~n0 devices
+@echo     %~n0 services
+@echo     %~n0 certificates
+@echo     %~n0 eject
+@echo     %~n0 control ^<feature^>
+@echo       Available features are:
+@echo         - users
+@echo         - taskbar
+@echo         - troubles
 @echo.
 @goto :eof
 
-:eject
-@start rundll32 shell32.dll,Control_RunDLL hotplug.dll
+:users
+@start lusrmgr.msc
+@goto :eof
+
+:disks
+@start diskmgmt.msc
+@goto :eof
+
+:devices
+@start devmgmt.msc
 @goto :eof
 
 :services
 @start services.msc
+@goto :eof
+
+:certificates
+@start certmgr.msc
+@goto :eof
+
+:eject
+@start rundll32.exe shell32.dll,Control_RunDLL hotplug.dll
+@goto :eof
+
+:control
+@shift
+@if "%1" == "users" goto :control_users
+@if "%1" == "taskbar" goto :control_taskbar
+@if "%1" == "troubles" goto :control_troubles
+@goto :help
+
+:control_users
+@control.exe /name Microsoft.UserAccounts
+@goto :eof
+
+:control_taskbar
+@control.exe /name Microsoft.TaskbarandStartMenu
+@goto :eof
+
+:control_troubles
+@control.exe /name Microsoft.Troubleshooting
 @goto :eof
 
 :eof
