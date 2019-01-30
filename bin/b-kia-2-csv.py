5f6a4f46223d1c0c1cb568199de3cec704bddc95
Antonio Alvarado H <tnotstar@gmail.com>
2019-01-30 19:16:45 +0100

    first runnable version of `b-kia-2-csv.py` parser
    - updated `b-kia-2-csv.py` to parse files and create the .csv

--- bin/b-kia-2-csv.py
+++ bin/b-kia-2-csv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-# Copyright (c) 2017-2018 Antonio Alvarado Hernández - All rights reserved
+# Copyright (c) 2017-2019 Antonio Alvarado Hernández - All rights reserved
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -22,116 +22,128 @@ import html.parser as html_parser
 
 import re
 import sys
+import csv
+import glob
+import html.parser as html_parser
 
 
-RX_TITLE_ID = re.compile(r"movimientos\s+de\s+la\s+cuenta\s+(.+)", re.I)
-RX_TITLE_DATE = re.compile(r"reporte\s+generado\s+a\s+(.+)\.", re.I)
+ST_READY    = 0
+ST_IN_HTML  = 1
+ST_IN_BODY  = 2
+ST_IN_TABLE = 3
+ST_IN_THEAD = 4
+ST_IN_THTR  = 5
+ST_IN_TRTH  = 6
+ST_IN_TBODY = 7
+ST_IN_TBTR  = 8
+ST_IN_TRTD  = 9
+ST_ENDED    = -1
 
-ST_READY       = 0
-ST_IN_TABLE    = 1
-ST_IN_TTTLID   = 2
-ST_IN_TTTLDATE = 3
-ST_IN_TTABLE   = 4
-ST_IN_TBODY    = 5
-ST_IN_TROW     = 6
-ST_IN_TCOLUMN  = 7
+NS_HTML = r"http://www.w3.org/TR/REC-html40"
+NS_EXCEL = r"urn:schemas-microsoft-com:office:excel"
+NS_OFFICE = r"urn:schemas-microsoft-com:office:office"
+
+EXCEL_ES = csv.excel
+EXCEL_ES.delimiter = ";"
 
 
-class HTMLStatementParser(html_parser.HTMLParser, object):
-    """TODO: Blah, blah, blah, ..."""
+class HTMLStatementParser(html_parser.HTMLParser):
+    """Parse a statement exported as an Excel XML Export file."""
 
-    def __init__(self):
-        super(HTMLStatementParser, self).__init__()
+    def __init__(self, writer):
+        super().__init__()
+        self._writer = writer
         self._state = ST_READY
-        self._account_no = None
-        self._report_dt = None
-        self._current = None
+        self._in_title = True
+        self._in_header = True
+        self._row = None
+        self._title = None
+        self._header = None
 
     def handle_starttag(self, tag, attrs):
-        if self._state == ST_READY:
-            if tag == "table":
-                self._state = ST_IN_TABLE
-        elif self._state == ST_IN_TABLE:
-            if tag == "span":
-                xid = dict(attrs).get('id', None)
-                if xid == "tituloDocumento":
-                    self._state = ST_IN_TTTLID
-                elif xid == "tituloFecha":
-                    self._state = ST_IN_TTTLDATE
-            elif tag == "table":
-                self._state = ST_IN_TTABLE
-        elif self._state == ST_IN_TTABLE:
-            if tag == "tbody":
-                self._state = ST_IN_TBODY
-        elif self._state == ST_IN_TBODY:
-            if tag == "tr":
-                self._state = ST_IN_TROW
-        elif self._state == ST_IN_TROW:
-            if tag == "td":
-                self._state = ST_IN_TCOLUMN
-
-    def handle_endtag(self, tag):
-        if self._state == ST_IN_TCOLUMN:
-            if tag == "td":
-                self._state = ST_IN_TROW
-        elif self._state == ST_IN_TROW:
-            if tag == "tr":
-                self._state = ST_IN_TBODY
-        elif self._state == ST_IN_TBODY:
-            if tag == "tbody":
-                self._state = ST_IN_TTABLE
-        elif self._state == ST_IN_TTABLE:
-            if tag == "table":
-                self._state = ST_IN_TABLE
-        elif self._state in (ST_IN_TTTLID, ST_IN_TTTLDATE):
-            if tag == "span":
-                self._state = ST_IN_TABLE
-        elif self._state == ST_IN_TABLE:
-            if tag == "table":
-                self._state = ST_READY
+        attrs = dict(attrs)
+        if self._state == ST_READY and tag == "html":
+            namespaces = attrs.values()
+            if all([NS_HTML in namespaces, NS_EXCEL in namespaces,
+                    NS_OFFICE in namespaces]):
+                self._state = ST_IN_HTML
+        elif self._state == ST_IN_HTML and tag == "body":
+            self._state = ST_IN_BODY
+        elif self._state == ST_IN_BODY and tag == "table":
+            self._state = ST_IN_TABLE
+        elif self._state == ST_IN_TABLE and tag == "thead":
+            self._state = ST_IN_THEAD
+        elif self._state == ST_IN_THEAD and tag == "tr":
+            self._row = list()
+            self._state = ST_IN_THTR
+        elif self._state == ST_IN_THTR and tag == "th":
+            self._state = ST_IN_TRTH
+        elif self._state == ST_IN_TABLE and tag == "tbody":
+            self._state = ST_IN_TBODY
+        elif self._state == ST_IN_TBODY and tag == "tr":
+            self._row = list()
+            self._state = ST_IN_TBTR
+        elif self._state == ST_IN_TBTR and tag == "td":
+            self._state = ST_IN_TRTD
 
     def handle_data(self, data):
-        if self._state == ST_IN_TTTLID:
-            match = RX_TITLE_ID.search(data)
-            if match:
-                self._account_no = match.group(1)
-            print("###", self._account_no)
-        elif self._state == ST_IN_TTTLDATE:
-            match = RX_TITLE_DATE.search(data)
-            if match:
-                self._report_date = match.group(1)
-            print("###", self._report_date)
-        elif self._state == ST_IN_TCOLUMN:
-            print("===", data)
+        if self._state == ST_IN_TRTH:
+            self._row.append(data)
+        elif self._state == ST_IN_TRTD:
+            self._row.append(data)
+
+    def handle_endtag(self, tag):
+        if self._state == ST_IN_TRTD and tag == "td":
+            self._state = ST_IN_TBTR
+        elif self._state == ST_IN_TBTR and tag == "tr":
+            if self._row:
+                self._writer.writerow(self._row)
+                if self._in_header:
+                    self._header = self._row[:]
+                    self._in_header = False
+            self._state = ST_IN_TBODY
+        elif self._state == ST_IN_TBODY and tag == "tbody":
+            self._state = ST_IN_TABLE
+        elif self._state == ST_IN_TRTH and tag == "th":
+            self._state = ST_IN_THTR
+        elif self._state == ST_IN_THTR and tag == "tr":
+            if self._row:
+                self._writer.writerow(self._row)
+                if self._in_title:
+                    self._title = self._row[:]
+            self._state = ST_IN_THEAD
+        elif self._state == ST_IN_THEAD and tag == "thead":
+            self._in_title = False
+            self._state = ST_IN_TABLE
+        elif self._state == ST_IN_TABLE and tag == "table":
+            self._state = ST_IN_BODY
+        elif self._state == ST_IN_BODY and tag == "body":
+            self._state = ST_IN_HTML
+        elif self._state == ST_IN_HTML and tag == "html":
+            self._state = ST_ENDED
 
 
-def parse_statement_from_html(fname):
-    """TODO: Blah, blah, blah, ..."""
-    parser = HTMLStatementParser()
-    with open(fname) as stream:
-        buffer = stream.read()
-        parser.feed(buffer)
-        parser.close()
-    return None
+def create_output_datasheet(outfile, infiles):
+    with outfile and open(outfile, "w", newline="") or sys.stdout as output:
+        writer = csv.writer(output, dialect=EXCEL_ES)
+        for infile in [i for s in [glob.glob(p) for p in infiles] for i in s]:
+            parser = HTMLStatementParser(writer)
+            with open(infile, encoding="utf-8") as input:
+                buffer = input.read()
+                parser.feed(buffer)
+                parser.close()
+            print(infile, parser._header)
 
-
-#if __name__ == "__main__":
-#    stmt = parse_statement_from_html(sys.argv[1])
-#    print(stmt)
-
-def parse_html_input_files(input_files):
-    print(input_files)
-
-
-DESCRIPT = """
-Convert given input files in b-kia format to just one .csv output file
-"""
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description=DESCRIPT)
-    parser.add_argument("input_file", type=str, nargs="+", metavar="input_files",
-        help="the input file names")
+    descrpt = "Convert a list of b-kia's input files to a merged .csv one."
+    parser = ArgumentParser(description=descrpt)
+    parser.add_argument("-o", "--output", type=str, metavar="output_file",
+        help="the name of the output file")
+    parser.add_argument("inputs", type=str, metavar="input_file", nargs="+",
+        help="a bunch of input filenames to read for them")
+
     args = parser.parse_args()
-    parse_html_input_files(args.input_files)
+    create_output_datasheet(args.output, args.inputs)
 
 # EOF
