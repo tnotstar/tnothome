dbf7d735e6b7ce518842fb36f959cc85d2e53c5c
Antonio Alvarado H <tnotstar@gmail.com>
2019-03-28 19:00:49 +0100

    upgraded `trpath` to my current practices
    - removed bad @echo off statement
    - changed read clipboard via `getclip` to `Get-Clipboard` from PS
    - updated the copyright line with new years

--- bin/trpath.cmd
+++ bin/trpath.cmd
@@ -1,55 +1,50 @@
-@echo off
-rem ----------------------------------------------------------------------------
-rem Copyright (c) 2011, 2012 Antonio Alvarado Hernández - All rights reserved
-rem ----------------------------------------------------------------------------
-rem
-rem   Licensed under the Apache License, Version 2.0 (the "License");
-rem   you may not use this file except in compliance with the License.
-rem   You may obtain a copy of the License at
-rem
-rem       http://www.apache.org/licenses/LICENSE-2.0
-rem
-rem   Unless required by applicable law or agreed to in writing, software
-rem   distributed under the License is distributed on an "AS IS" BASIS,
-rem   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-rem   See the License for the specific language governing permissions and
-rem   limitations under the License.
-rem
-rem ----------------------------------------------------------------------------
-rem $Id$
-rem ----------------------------------------------------------------------------
+@rem -*- coding: utf-8 -*-
+@rem
+@rem Copyright (c) 2011-2019 Antonio Alvarado Hernández - All rights reserved
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem     http://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
 
+@setlocal enableExtensions
+@setlocal disableDelayedExpansion
 
-setlocal enableextensions
-setlocal disabledelayedexpansion
+@set NAME=%~n0 && @shift
 
-set NAME=%~n0 && shift
+@set SED_PROGRAM=sed
 
-set SED_PROGRAM=sed
+@if [%0] == [-u] @set SED_OPTIONS=-e "s/\\/\//g"
+@if [%0] == [-w] @set SED_OPTIONS=-e "s/\//\\/g"
 
-if [%0] == [-u] set SED_OPTIONS=-e "s/\\/\//g"
-if [%0] == [-w] set SED_OPTIONS=-e "s/\//\\/g"
+@if not defined SED_OPTIONS @goto :syntax
 
-if not defined SED_OPTIONS goto :syntax
+@if [%1] == []  @set READ_COMMAND=powershell -NoProfile -Command Get-Clipboard
+@if [%1] == [-] @set READ_COMMAND=cat
 
-if [%1] == []  set READ_COMMAND=getclip
-if [%1] == [-] set READ_COMMAND=cat
-
-if not defined READ_COMMAND set READ_COMMAND=echo %1
+@if not defined READ_COMMAND @set READ_COMMAND=echo %1
 
 :execute
-%READ_COMMAND% | %SED_PROGRAM% %SED_OPTIONS%
-goto :eof
+@%READ_COMMAND% | @%SED_PROGRAM% %SED_OPTIONS%
+@goto :eof
 
 :syntax
-echo. 1>&2
-echo Fatal: invalid command line arguments 1>&2
-echo. 1>&2
-echo   Syntax: %NAME% [-u^|-w] 1>&2
-echo           %NAME% [-u^|-w] - 1>&2
-echo           %NAME% [-u^|-w] ^<path-string^> 1>&2
-goto :eof
+@echo. 1>&2
+@echo Fatal: invalid command line arguments 1>&2
+@echo. 1>&2
+@echo   Syntax: %NAME% [-u^|-w] 1>&2
+@echo           %NAME% [-u^|-w] - 1>&2
+@echo           %NAME% [-u^|-w] ^<path-string^> 1>&2
+@goto :eof
 
-endlocal
+@endlocal
 
 :eof
