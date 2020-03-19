ad009c79ae17a7d21b615cd63e6641b83e067fdf
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-03-19 21:01:15 +0100

    a script to start/stop `openresty`
    - added a `cmd` batch file for `openresty` from `scoop`

---
+++ bin/myresty.cmd
@@ -0,0 +1,53 @@
+@rem -*- coding: utf-8 -*-                                                                   
+@rem                                                                                         
+@rem Copyright (c) 2020 Antonio Alvarado Hernández - All rights reserved                     
+@rem                                                                                         
+@rem Licensed under the Apache License, Version 2.0 (the "License");                         
+@rem you may not use this file except in compliance with the License.                        
+@rem You may obtain a copy of the License at                                                 
+@rem                                                                                         
+@rem     http://www.apache.org/licenses/LICENSE-2.0                                          
+@rem                                                                                         
+@rem Unless required by applicable law or agreed to in writing, software                     
+@rem distributed under the License is distributed on an "AS IS" BASIS,                       
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.                
+@rem See the License for the specific language governing permissions and                     
+@rem limitations under the License.                                                          
+@rem                                                                                         
+                                      
+									  
+@set RESTY_PATH=.
+@for /F %%p in ('scoop which openresty') do @set RESTY_PATH=%%~dpp
+
+@if "%1" == "" goto :usage
+@if "%1" == "-h" goto :usage
+@if "%1" == "/?" goto :usage
+
+@if "%1" == "stop" goto :stop
+@if "%1" == "start" goto :start
+
+:start
+@echo Starting up from "%RESTY_PATH%"...
+@start /I /MIN openresty -p %RESTY_PATH%
+@if %errorlevel% neq 0 @goto :error
+@goto :eof
+
+:stop
+@echo Shutting down...
+@openresty -p %RESTY_PATH% -s stop
+@if %errorlevel% neq 0 @goto :error
+@goto :eof
+
+:usage
+@echo Oops: invalid command line syntax
+@echo.
+@echo C:\^> %~nx0 start
+@echo C:\^> %~nx0 stop
+@echo.
+@goto :eof
+
+:error
+@echo Oops: something is wrong (errorlevel=%errorlevel%)^!^!
+@goto :eof
+
+:eof
