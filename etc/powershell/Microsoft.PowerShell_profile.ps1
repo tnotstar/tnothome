d17156a496b41f7a589fe5818b1ef992d561369b
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-03-25 08:51:51 +0100

    fixed bad copyright date
    - updated the copyright year

--- etc/powershell/Microsoft.PowerShell_profile.ps1
+++ etc/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2019 Antonio Alvarado Hernández - All rights reserved
+# Copyright (c) 2020 Antonio Alvarado Hernández - All rights reserved
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -12,8 +12,6 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-#
-# Microsoft.PowerShell_profile.ps1
 #
 
 
