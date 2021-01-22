06532dc55d39c9fdf0b2230228dd917b09b9821b
Antonio Alvarado Hernández <tnotstar@gmail.com>
2021-01-22 14:19:54 +0100

    updated command arguments for `shui.cmd`
    - removed `control` subcommands
    - added `system` option
    - updated copyright remarks

--- bin/shui.cmd
+++ bin/shui.cmd
@@ -1,6 +1,6 @@
 @rem -*- coding: utf-8 -*-
 @rem
-@rem Copyright (c) 2012-2019 Antonio Alvarado Hernández - All rights reserved
+@rem Copyright (c) 2012-2020 Antonio Alvarado Hernández - All rights reserved
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
@@ -15,48 +15,50 @@
 @rem limitations under the License.
 @rem
 
-@if "%1" == "users" goto :users
-@if "%1" == "disks" goto :disks
-@if "%1" == "devices" goto :devices
+@if "%1" == "system" goto :system
+@if "%1" == "properties" goto :properties
+@if "%1" == "policies" goto :policies
 @if "%1" == "services" goto :services
 @if "%1" == "programs" goto :programs
-@if "%1" == "policies" goto :policies
-@if "%1" == "properties" goto :properties
-@if "%1" == "certificates" goto :certificates
-@if "%1" == "control"  goto :control
+@if "%1" == "user" goto :user
+@if "%1" == "users" goto :users
+@if "%1" == "devices" goto :devices
+@if "%1" == "disks" goto :disks
 @if "%1" == "eject" goto :eject
+@if "%1" == "taskbar" goto :taskbar
+@if "%1" == "troubles" goto :troubles
+@if "%1" == "certificates" goto :certificates
 @goto :help
 
 :help
 @echo Show some Windows GUI dialogs from the command line
 @echo.
-@echo     %~n0 users
-@echo     %~n0 disks
-@echo     %~n0 devices
+@echo     %~n0 system
+@echo     %~n0 properties
+@echo     %~n0 policies
 @echo     %~n0 services
 @echo     %~n0 programs
-@echo     %~n0 policies
-@echo     %~n0 properties
-@echo     %~n0 certificates
-@echo     %~n0 control ^<feature^>
-@echo       Available features are:
-@echo         - users
-@echo         - taskbar
-@echo         - troubles
+@echo     %~n0 user
+@echo     %~n0 users
+@echo     %~n0 devices
+@echo     %~n0 disks
 @echo     %~n0 eject
+@echo     %~n0 taskbar
+@echo     %~n0 troubles
+@echo     %~n0 certificates
 @echo.
 @goto :eof
 
-:users
-@start lusrmgr.msc
+:system
+@control.exe system
 @goto :eof
 
-:disks
-@start diskmgmt.msc
+:properties
+@control.exe sysdm.cpl
 @goto :eof
 
-:devices
-@start devmgmt.msc
+:policies:
+@start secpol.msc
 @goto :eof
 
 :services
@@ -64,42 +66,39 @@
 @goto :eof
 
 :programs:
-@start appwiz.cpl
+@control.exe appwiz.cpl
 @goto :eof
 
-:policies:
-@start secpol.msc
-@goto :eof
-
-:properties
-@control.exe sysdm.cpl
-@goto :eof
-
-:certificates
-@start certmgr.msc
-@goto :eof
-
-:control
-@shift
-@if "%1" == "users" goto :control_users
-@if "%1" == "taskbar" goto :control_taskbar
-@if "%1" == "troubles" goto :control_troubles
-@goto :help
-
-:control_users
+:user
 @control.exe /name Microsoft.UserAccounts
 @goto :eof
 
-:control_taskbar
-@control.exe /name Microsoft.TaskbarandStartMenu
+:users
+@start lusrmgr.msc
 @goto :eof
 
-:control_troubles
-@control.exe /name Microsoft.Troubleshooting
+:devices
+@start devmgmt.msc
+@goto :eof
+
+:disks
+@start diskmgmt.msc
 @goto :eof
 
 :eject
 @start rundll32.exe shell32.dll,Control_RunDLL hotplug.dll
+@goto :eof
+
+:taskbar
+@control.exe /name Microsoft.TaskbarandStartMenu
+@goto :eof
+
+:troubles
+@control.exe /name Microsoft.Troubleshooting
+@goto :eof
+
+:certificates
+@start certmgr.msc
 @goto :eof
 
 :eof
