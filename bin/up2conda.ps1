eec283a9bd1b1b1fc4ab0e2550f40c457faf3ec1
Antonio Alvarado Hernández <tnotstar@gmail.com>
2022-05-28 10:00:57 +0200

    added support for running `up2conda.ps1` from shell
    - also included minor formatting fixes

--- bin/up2conda.ps1
+++ bin/up2conda.ps1
@@ -1,31 +1,37 @@
 #!/usr/bin/env pwsh
 
-# Copyright 2020-2021, Antonio Alvarado Hernández
+# Copyright 2020-2022, Antonio Alvarado Hernández
 
 try {
-    Get-Command Invoke-Conda -ErrorAction Stop | Out-Null
+    $Conda = Get-Command conda -ErrorAction Stop
 
+    if (!(Get-Command Invoke-Conda -ErrorAction SilentlyContinue)) {
+        (& $Conda "shell.powershell" "hook") | Out-String | Invoke-Expression
+    }
+
+    Write-Host "Running 'conda' updating routines...`n" -ForegroundColor Yellow
     try {
-        Write-Host ">> Updating ``conda`` package..." -ForegroundColor DarkYellow
+        Write-Host "> Updating ``conda`` package..." -ForegroundColor DarkYellow
         Invoke-Conda update -y conda
 
-        Write-Host ">> Updating ``base`` environment..." -ForegroundColor DarkYellow
+        Write-Host "> Updating ``base`` environment..." -ForegroundColor DarkYellow
         Invoke-Conda update -y --all
 
-        $environments = Invoke-Conda info --json | ConvertFrom-Json
-        foreach ($base in $environments.envs) {
-            if ("$base" -ne "$Env:CONDA_PREFIX") {
+        Write-Host "> Updating remaining environments:`n" -ForegroundColor DarkYellow
+        $info = Invoke-Conda info --json | ConvertFrom-Json
+        foreach ($base in $info.envs) {
+            if ($base -ne $info.root_prefix) {
                 $name = [IO.Path]::GetFileNameWithoutExtension($base)
-                Write-Host ">> Updating environment ``$name``..." -ForegroundColor DarkYellow
+                Write-Host "   * Updating environment ``$name``..." -ForegroundColor Cyan
                 Invoke-Conda update -y --name "$name" --all
             }
         }
-        Write-Host ">> All updates are finished ok!!" -ForegroundColor Green
+        Write-Host "Info: All updates are finished ok!!" -ForegroundColor Green
     }
     catch {
         Write-Error "Oops: Conda update command has been failed"
     }
 }
 catch {
-    Write-Error "Oops: Conda shell integration not found. Try running `condavars` first"
+    Write-Error "Oops: 'conda' executable is not in your PATH. Try (re)installing 'miniconda'"
 }
