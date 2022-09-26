ec45d4f1af2cf69fdc746649558ca5bf3628d642
Antonio Alvarado Hernández <tnotstar@gmail.com>
2022-09-26 10:13:18 +0200

    added monorepo configuration for `golang`

--- etc/fish/conf.d/golang.fish
+++ etc/fish/conf.d/golang.fish
@@ -2,6 +2,9 @@
 # ~/Local/etc/fish/conf.d/golang.fish
 #
 
+set -gx GOPATH "$HOME/Library/Go:$HOME/Workspaces/Go"
+
 fish_add_path -gaP ~/Library/Go/bin
+fish_add_path -gaP ~/Workspaces/Go/bin
 
 # EOF
