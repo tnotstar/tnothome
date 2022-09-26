6985305d3b7f513d133bc8cbeecd1da64de7a6ad
Antonio Alvarado Hernández <tnotstar@gmail.com>
2022-09-26 10:10:47 +0200

    updated alias for running `charles3`

--- etc/fish/conf.d/aliases.fish
+++ etc/fish/conf.d/aliases.fish
@@ -3,7 +3,7 @@
 #
 
 alias start=xdg-open
-alias charles="JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64 ~/Library/Charles/bin/charles"
+alias charles="JRE_HOME=/usr/lib/jvm/java-8-openjdk/jre ~/Library/Charles/bin/charles"
 
 alias rm='/usr/bin/rm -i'
 alias mv='/usr/bin/mv -i'
