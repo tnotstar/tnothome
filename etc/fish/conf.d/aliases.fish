b12d7b8f963ccfa9dfb64e6d435bbbd6062e56db
Antonio Alvarado Hernández <tnotstar@gmail.com>
2022-12-26 15:21:32 +0100

    fixed `LS_OPTIONS` in `fish` configuration

--- etc/fish/conf.d/aliases.fish
+++ etc/fish/conf.d/aliases.fish
@@ -3,8 +3,8 @@
 #
 
 alias start=xdg-open
-alias charles="JRE_HOME=/usr/lib/jvm/java-8-openjdk/jre ~/Library/Charles/bin/charles"
 
+alias ls='/usr/bin/ls $LS_OPTIONS'
 alias rm='/usr/bin/rm -i'
 alias mv='/usr/bin/mv -i'
 alias cp='/usr/bin/cp -i'
@@ -12,5 +12,7 @@ alias cp='/usr/bin/cp -i'
 alias vi='nvim'
 alias vim='nvim'
 alias gvim='nvim-qt'
+
+alias charles="JRE_HOME=/usr/lib/jvm/java-8-openjdk/jre ~/Library/Charles/bin/charles"
 
 # EOF
--- etc/fish/conf.d/environ.fish
+++ etc/fish/conf.d/environ.fish
@@ -2,6 +2,8 @@
 # ~/Local/etc/fish/conf.d/variables.fish
 #
 
+set -gx LS_OPTIONS --group-directories-first --color=auto
+
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 
