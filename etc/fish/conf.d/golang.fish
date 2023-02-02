44b654019888d3954ddf9d8905b9afefb430b45d
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-02-02 11:01:47 +0100

    updated `golang` workspace configuration

--- etc/fish/conf.d/golang.fish
+++ etc/fish/conf.d/golang.fish
@@ -2,9 +2,9 @@
 # ~/Local/etc/fish/conf.d/golang.fish
 #
 
-set -gx GOPATH "$HOME/Library/Go:$HOME/Workspaces/Go"
+set -gx GOPATH "$HOME/Library/Golang:$HOME/Workspaces/Golang"
 
-fish_add_path -gaP ~/Library/Go/bin
-fish_add_path -gaP ~/Workspaces/Go/bin
+fish_add_path -gaP ~/Library/Golang/bin
+fish_add_path -gaP ~/Workspaces/Golang/bin
 
 # EOF
