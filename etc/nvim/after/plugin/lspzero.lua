8a4b87194677b7cb2dd7df19c56ea10754f122c3
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-03-27 02:14:28 +0200

    fixed `lsp-zero` & `treesitter` nvim's configuration

--- etc/nvim/after/plugin/lspzero.lua
+++ etc/nvim/after/plugin/lspzero.lua
@@ -2,12 +2,41 @@
 -- ~/Local/etc/nvim/lua/my/lspzero.lua
 --
 
-local lsp = require('lsp-zero').preset('recommended')
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.set_preferences({
+  suggest_lsp_servers = true,
+})
+
+lsp.ensure_installed({
+  'gopls',
+  'pyright',
+  'tsserver',
+  'rust_analyzer',
+})
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.on_attach(function(_, bufnr)
   lsp.default_keymaps({ buffer = bufnr })
 end)
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mapping = lsp.defaults.cmp_mappings({
+  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mapping
+})
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
