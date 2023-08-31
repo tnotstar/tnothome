642e22fb350b7822dd60d4b20be5af2258138dfe
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-08-31 10:04:19 +0200

    workaround for bad `monospace` to `fira code` match of `neovide` font

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -380,7 +380,7 @@ if vim.g.neovide then
   vim.g.neovide_transparency = 0.9
   vim.g.neovide_hide_mouse_when_typing = true
 
-  vim.opt.guifont = 'Monospace:h10'
+  vim.opt.guifont = 'Fira\\ Code:h10'
   vim.opt.linespace = 0
 
   vim.cmd.colorscheme('rose-pine')
