9560be4e5197959f7677d2694b040f3baf93a384
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-09-03 11:53:36 +0200

    added a funny `opentowork.ts` script!!

---
+++ bin/opentowork.ts
@@ -0,0 +1,14 @@
+#!/bin/sh
+':' //; exec `command -v deno` run --allow-net "$0" "$@"
+
+// Copyright (c) 2023, Antonio Alvarado Hernández <tnotstar@gmail.com>
+
+const payload = [ 73, 39, 109, 32, 35, 111, 112, 101, 110, 84, 111, 87, 111, 114, 107, 33, 33 ]
+
+const msg = await fetch('https://httpbin.org/post', {
+    method: 'POST',
+    body: JSON.stringify(payload),
+}).then(res => res.json());
+
+console.log(String.fromCharCode(...msg.json));
+
