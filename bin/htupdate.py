f443433a16464f222050ccaed92c83e05c0700b4
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-09-04 11:23:56 +0200

    fixed minor issue in unix-based implementation of `htupdate.py`

--- bin/htupdate.py
+++ bin/htupdate.py
@@ -1,21 +1,7 @@
 #!/usr/bin/env python3
-#
-# Copyright (c) 2016-2018 Antonio Alvarado Hernández - All rights reserved
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 # -*- coding: utf-8 -*-
 
+# Copyright 2016-2023, Antonio Alvarado Hernández
 
 from calendar import timegm
 from datetime import datetime
@@ -42,13 +28,11 @@ if __name__ == "__main__":
                 command.append(date_dt.strftime("%H:%M:%S"))
                 output = check_output(command).decode()
             else:
-                command = "/usr/bin/date --utc -Rs".split()
-                command.append(srvdate)
+                command = "/usr/bin/date --utc --iso-8601 -s".split()
+                command.append(date_dt.isoformat())
                 output = check_output(command).decode()
     except HTTPError as ex:
         print("Oops: can't retrieve date from Google ({})".format(str(ex)))
 
     except CalledProcessError as ex:
         print("Oops: can't call `date` command ({})".format(str(ex)))
-
-# EOF
