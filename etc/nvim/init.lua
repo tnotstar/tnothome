667d9c90e88e46fccf24e47af6280964adde9981
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-11-10 13:57:05 +0100

    removed unused plugins in `neovim` configuration

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -54,29 +54,19 @@ require('packer').startup(function(use)
       {'hrsh7th/cmp-path'},         -- Optional
       {'hrsh7th/cmp-buffer'},       -- Optional
       {'saadparwaiz1/cmp_luasnip'}, -- Optional
-
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
     }
-  })
-
-  -- Treesitter configurations and abstraction layer for Neovim
-  use({
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
   })
 
   -- Rosé Pine, all natural pine, faux fur and a bit of soho vibes
   use({
-    'rose-pine/neovim',
-    as = 'rose-pine',
+    'rose-pine/neovim', as = 'rose-pine',
   })
 
   -- A blazing fast and easy to configure neovim statusline plugin
   use({
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    'nvim-lualine/lualine.nvim', requires = {
+       'nvim-tree/nvim-web-devicons', opt = true,
+     },
   })
 
   -- Automatically adjusts 'shiftwidth' and 'expandtab' heuristically
@@ -98,12 +88,6 @@ require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} },
   })
 
-  -- File manager for Neovim powered by nnn.
-  use({
-    'luukvbaal/nnn.nvim',
-    config = function() require('nnn').setup() end
-  })
-
   -- Neovim plugin for GitHub Copilot
   use('github/copilot.vim')
 
@@ -113,45 +97,6 @@ end)
 -- ---
 -- set up plugins options
 --
-
--- settings of `lualine` plugin
---
-require('lualine').setup()
-
--- settings of `treesitter` plugin
---
-require('nvim-treesitter.configs').setup({
-    -- A list of parser names, or 'all' (the four listed parsers should always be
-    -- installed)
-    ensure_installed = {
-      'vim', 'c', 'cpp', 'lua', 'python', 'javascript', 'typescript', 'go', 'rust',
-    },
-
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
-
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed
-    -- locally
-    auto_install = true,
-
-    -- Enable smart-indentation
-    indent = {
-        enable = true,
-    },
-
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same
-        -- time. Set this to `true` if you depend on 'syntax' being enabled
-        -- (like for indentation). Using this option may slow down your editor,
-        -- and you may see some duplicate highlights. Instead of true it can
-        -- also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-})
 
 -- settings of `mason` plugin
 --
@@ -172,7 +117,6 @@ lsp.ensure_installed({
   'pyright',
   'denols',
   'tsserver',
-  'rust_analyzer',
 })
 
 local lspconfig = require('lspconfig')
@@ -246,23 +190,20 @@ vim.g.copilot_filetypes = {
   ['javascript'] = true,
   ['typescript'] = true,
   ['lua'] = true,
-  ['c'] = true,
-  ['cpp'] = true,
-  ['rust'] = true,
   ['go'] = true,
   ['python'] = true,
 }
-
 vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
+-- settings of `lualine` plugin
+--
+require('lualine').setup()
 
 -- ---
 -- set up colors scheme
 --
-
 vim.cmd.colorscheme('rose-pine')
-
--- set transparent background
+                                       -- set transparent background
 vim.cmd.highlight({ 'Normal', 'guibg=none', 'ctermbg=none' })
 vim.cmd.highlight({ 'NormalFloat', 'guibg=none', 'ctermbg=none' })
 
@@ -270,26 +211,6 @@ vim.cmd.highlight({ 'NormalFloat', 'guibg=none', 'ctermbg=none' })
 -- ---
 -- set up personal keyboard mappings
 --
-
-vim.g.mapleader = ' '
-
-vim.keymap.set('n', '<Leader>nx', vim.cmd.NnnExplorer)
-vim.keymap.set('n', '<Leader>nn', function() vim.cmd.NnnPicker('%:p:h') end)
-
-local telescope_builtin = require('telescope.builtin')
-
-vim.keymap.set('n', '<Leader>tf', telescope_builtin.find_files, {})
-vim.keymap.set('n', '<Leader>tg', telescope_builtin.git_files, {})
-
-vim.keymap.set('n', '<Leader>gG', vim.cmd.Git)
-
-
--- ---
--- set up legacy key remappings
---
-                                       -- disable search highlighting
-vim.keymap.set('n', '<Leader>gh', '<ESC>:noh<CR>')
-
 vim.keymap.set('n', '<C-p>', ':bprevious<CR>')
 vim.keymap.set('n', '<C-n>', ':bnext<CR>')
 
@@ -307,6 +228,19 @@ vim.keymap.set('v', '<S-Del>', '"+x', { noremap=true })
 vim.keymap.set('n', '<C-V>', '"+gP', { noremap=true })
 vim.keymap.set('n', '<S-Insert>', '"+gP', { noremap=true })
 
+vim.g.mapleader = ' '
+
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<Leader>tf', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<Leader>tg', telescope_builtin.git_files, {})
+
+vim.keymap.set('n', '<Leader>gG', vim.cmd.Git)
+                                       -- disable search highlighting
+vim.keymap.set('n', '<Leader>gh', '<ESC>:noh<CR>')
+
+-- ---
+-- set up legacy key remappings
+--
 
 -- ---
 -- set up common options
@@ -376,7 +310,6 @@ end
 --
 
 if vim.g.neovide then
-
   vim.g.neovide_transparency = 0.9
   vim.g.neovide_hide_mouse_when_typing = true
 
@@ -384,7 +317,6 @@ if vim.g.neovide then
   vim.opt.linespace = 0
 
   vim.cmd.colorscheme('rose-pine')
-
 end
 
 -- EOF
