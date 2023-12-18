e71dcd419b55fffbd2717a36dd4fe8a1db39abe6
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-12-18 18:29:36 +0100

    updated `nvim` & `startship` configurations

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -6,47 +6,47 @@
 -- set up common options
 --
 
-vim.scriptencoding = 'utf-8'           -- set the encoding of the current script
+vim.scriptencoding = 'utf-8' -- set the encoding of the current script
 
 -- disable non-vim or non-lua extension languages
 --
-vim.g.loaded_python3_provider = 0      -- python3
-vim.g.loaded_ruby_provider = 0         -- ruby
-vim.g.loaded_node_provider = 0         -- node
-vim.g.loaded_perl_provider = 0         -- perl
+vim.g.loaded_python3_provider = 0 -- python3
+vim.g.loaded_ruby_provider = 0    -- ruby
+vim.g.loaded_node_provider = 0    -- node
+vim.g.loaded_perl_provider = 0    -- perl
 
 -- default legacy options
 --
-vim.opt.ruler = true                   -- show the cursor position all the time
-vim.opt.number = true                  -- display line numbers
-vim.opt.relativenumber = true          -- show numbers relative to current line
-vim.opt.cursorline = true              -- highlight the cursor line
-vim.opt.modeline = false               -- disable modelines for security
-vim.opt.showmatch = true               -- display matching brackets
-vim.opt.confirm = true                 -- ask to save changes before next one
-vim.opt.hidden = true                  -- allow hidden buffers when abandon them
-vim.opt.wrap = false                   -- by default, disable word wrap
-vim.opt.scrolloff = 5                  -- show some lines of context around the cursor
-vim.opt.history = 4096                 -- how many commands of history to recall
-vim.opt.cmdheight = 2                  -- set the command window height to 2
-vim.opt.display = 'truncate'           -- show @@@ in the last line, if truncated
-vim.opt.timeout = false                -- disable mappings timeout
-vim.opt.ttimeout = true                -- enable key code scan timeout
-                                       -- always use the global clipboard
-vim.opt.clipboard:append('unnamed')    -- put all text operations in the '*' register
+vim.opt.ruler = true                -- show the cursor position all the time
+vim.opt.number = true               -- display line numbers
+vim.opt.relativenumber = true       -- show numbers relative to current line
+vim.opt.cursorline = true           -- highlight the cursor line
+vim.opt.modeline = false            -- disable modelines for security
+vim.opt.showmatch = true            -- display matching brackets
+vim.opt.confirm = true              -- ask to save changes before next one
+vim.opt.hidden = true               -- allow hidden buffers when abandon them
+vim.opt.wrap = false                -- by default, disable word wrap
+vim.opt.scrolloff = 5               -- show some lines of context around the cursor
+vim.opt.history = 4096              -- how many commands of history to recall
+vim.opt.cmdheight = 2               -- set the command window height to 2
+vim.opt.display = 'truncate'        -- show @@@ in the last line, if truncated
+vim.opt.timeout = false             -- disable mappings timeout
+vim.opt.ttimeout = true             -- enable key code scan timeout
+-- always use the global clipboard
+vim.opt.clipboard:append('unnamed') -- put all text operations in the '*' register
 
 -- set up tabs/spaces options
 --
-vim.opt.expandtab = true               -- convert tabs to spaces
-vim.opt.tabstop = 4                    -- use 4 spaces for tabs
-vim.opt.shiftwidth = 4                 -- use 4 spaces for indentation
-vim.opt.softtabstop = 4                -- backspace key treat 4 spaces as 1 tab
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.tabstop = 4      -- use 4 spaces for tabs
+vim.opt.shiftwidth = 4   -- use 4 spaces for indentation
+vim.opt.softtabstop = 4  -- backspace key treat 4 spaces as 1 tab
 
 -- set up encoding and file format
 --
-vim.opt.encoding = 'utf-8'             -- the display encoding 
-vim.opt.fileencoding = 'utf-8'         -- the encoding written to file
-vim.opt.fileformat = 'unix'            -- use `unix` file formats by default
+vim.opt.encoding = 'utf-8'     -- the display encoding
+vim.opt.fileencoding = 'utf-8' -- the encoding written to file
+vim.opt.fileformat = 'unix'    -- use `unix` file formats by default
 
 -- enable file type, plugins and indent
 --
@@ -64,8 +64,8 @@ end
 
 -- terminal settings
 --
-vim.opt.termguicolors = true           -- enable gui colors for terminal
-vim.opt.visualbell = true              -- use visual bell instead of beeping
+vim.opt.termguicolors = true -- enable gui colors for terminal
+vim.opt.visualbell = true    -- use visual bell instead of beeping
 
 -- set transparent background
 --
@@ -74,8 +74,8 @@ vim.cmd.highlight({ 'NormalFloat', 'guibg=none', 'ctermbg=none' })
 
 -- set up mouse options
 --
-if vim.fn.has('mouse') then            -- if has('mouse'), enable mouse mode 
-  vim.opt.mouse = 'a'                  -- and copy&paste with the `shift` key
+if vim.fn.has('mouse') then -- if has('mouse'), enable mouse mode
+  vim.opt.mouse = 'a'       -- and copy&paste with the `shift` key
 end
 
 
@@ -88,30 +88,28 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<C-p>', ':bprevious<CR>')
 vim.keymap.set('n', '<C-n>', ':bnext<CR>')
 
-vim.keymap.set('n', '<A-j>', ':m+1<CR>==', { noremap=true })
-vim.keymap.set('i', '<A-j>', '<Esc>:m+1<CR>==gi', { noremap=true })
-vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap=true })
+vim.keymap.set('n', '<A-j>', ':m+1<CR>==', { noremap = true })
+vim.keymap.set('i', '<A-j>', '<Esc>:m+1<CR>==gi', { noremap = true })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true })
 
-vim.keymap.set('n', '<A-k>', ':m-2<CR>==', { noremap=true })
-vim.keymap.set('i', '<A-k>', '<Esc>:m-2<CR>==gi', { noremap=true })
-vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap=true })
+vim.keymap.set('n', '<A-k>', ':m-2<CR>==', { noremap = true })
+vim.keymap.set('i', '<A-k>', '<Esc>:m-2<CR>==gi', { noremap = true })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true })
 
-vim.keymap.set('v', '<C-C>', '"+y', { noremap=true })
-vim.keymap.set('v', '<C-Insert>', '"+y', { noremap=true })
+vim.keymap.set('v', '<C-C>', '"+y', { noremap = true })
+vim.keymap.set('v', '<C-Insert>', '"+y', { noremap = true })
 
-vim.keymap.set('v', '<C-X>', '"+x', { noremap=true })
-vim.keymap.set('v', '<S-Del>', '"+x', { noremap=true })
+vim.keymap.set('v', '<C-X>', '"+x', { noremap = true })
+vim.keymap.set('v', '<S-Del>', '"+x', { noremap = true })
 
-vim.keymap.set('n', '<C-V>', '"+gP', { noremap=true })
-vim.keymap.set('n', '<S-Insert>', '"+gP', { noremap=true })
+vim.keymap.set('n', '<C-V>', '"+gP', { noremap = true })
+vim.keymap.set('n', '<S-Insert>', '"+gP', { noremap = true })
 
 
 -- ---
--- load and preconfigure plugins
+-- load third-party plugins
 --
 
--- load `vim-plug` at first
---
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
@@ -125,9 +123,7 @@ Plug('rose-pine/neovim', {
 })
 
 -- A blazing fast and easy to configure neovim statusline plugin
-Plug('nvim-lualine/lualine.nvim', {
-  requires = { 'nvim-tree/nvim-web-devicons' }
-})
+Plug('nvim-lualine/lualine.nvim', { requires = { 'nvim-tree/nvim-web-devicons' } })
 
 -- Automatically adjusts 'shiftwidth' and 'expandtab' heuristically
 Plug('tpope/vim-sleuth')
@@ -141,16 +137,24 @@ Plug('tpope/vim-rhubarb')
 -- Git integration for buffers
 Plug('lewis6991/gitsigns.nvim')
 
--- Nvim Treesitter configurations and abstraction layer 
-Plug('nvim-treesitter/nvim-treesitter', {
-  run = ':TSUpdate'
-})
+-- Nvim Treesitter configurations and abstraction layer
+Plug('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
--- Mason, easily install and manage LSP servers, DAP servers,
--- linters, and formatters
+-- Quickstart configs for Nvim LSP
 Plug('neovim/nvim-lspconfig')
+
+-- Easily install and manage LSP servers, DAP servers, linters, and formatters.
 Plug('williamboman/mason.nvim')
 Plug('williamboman/mason-lspconfig.nvim')
+
+-- A completion plugins for neovim (coded in Lua)
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp')
+Plug('hrsh7th/cmp-nvim-lua')
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-buffer')
+
+Plug('VonHeikemen/lsp-zero.nvim', { branch = 'v3.x' })
 
 -- Telescope for Find, Filter, Preview, Pick, ...
 Plug('nvim-telescope/telescope.nvim', { branch = '0.1.x' })
@@ -160,178 +164,145 @@ Plug('github/copilot.vim')
 
 vim.call('plug#end')
 
----- load rest of plugins
-----
---require('packer').startup(function(use)
---
---
---
---  -- A starting point to setup some lsp related features in neovim
---  use({
---    'VonHeikemen/lsp-zero.nvim',
---    branch = 'v2.x',
---    requires = {
---      -- LSP Support
---      {'neovim/nvim-lspconfig'},             -- Required
---      {'williamboman/mason.nvim'},           -- Optional
---      {'williamboman/mason-lspconfig.nvim'}, -- Optional
---
---      -- Autocompletion
---      {'hrsh7th/nvim-cmp'},         -- Required
---      {'hrsh7th/cmp-nvim-lsp'},     -- Required
---      {'hrsh7th/cmp-nvim-lua'},     -- Optional
---      {'hrsh7th/cmp-path'},         -- Optional
---      {'hrsh7th/cmp-buffer'},       -- Optional
---      {'saadparwaiz1/cmp_luasnip'}, -- Optional
---    }
---  })
---
---end)
---
---
 -- ---
--- set up plugins options
+-- set up plugins configurations
 --
-
--- settings of `lualine` plugin
---
-require('lualine').setup()
 
 -- set up colors scheme
 --
 vim.cmd.colorscheme('rose-pine')
 
+-- settings of `lualine` plugin
+--
+require('lualine').setup()
+
 -- settings of `nvim-treesitter` plugin
 --
 require('nvim-treesitter.configs').setup({
-    -- A list of parser names, or 'all' (the four listed parsers should always be
-    -- installed)
-    ensure_installed = {
-      'vim', 'lua', 'go', 'python', 'javascript', 'typescript',
-    },
+  -- A list of parser names, or 'all' (the four listed parsers should always be
+  -- installed)
+  ensure_installed = {
+    'vim', 'lua', 'go', 'python', 'javascript', 'typescript',
+  },
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed
-    -- locally
-    auto_install = true,
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed
+  -- locally
+  auto_install = true,
 
-    -- Enable smart-indentation
-    indent = {
-        enable = true,
-    },
+  -- Enable smart-indentation
+  indent = {
+    enable = true,
+  },
 
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same
-        -- time. Set this to `true` if you depend on 'syntax' being enabled
-        -- (like for indentation). Using this option may slow down your editor,
-        -- and you may see some duplicate highlights. Instead of true it can
-        -- also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same
+    -- time. Set this to `true` if you depend on 'syntax' being enabled
+    -- (like for indentation). Using this option may slow down your editor,
+    -- and you may see some duplicate highlights. Instead of true it can
+    -- also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
 })
+
+local lspzero = require('lsp-zero')
+local lspconf = require('lspconfig')
+local lsputil = require('lspconfig.util')
+
+-- settings of `lspzero` plugin
+--
+lspzero.on_attach(function(_, bufnr)
+  lspzero.default_keymaps({ buffer = bufnr })
+
+  local options = { noremap = true, buffer = bufnr }
+
+  -- enable completion trigger by <C-x><C-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- enable custom keyboard mappings
+  vim.keymap.set('n', '<Leader>K', vim.lsp.buf.signature_help, options)
+end)
 
 -- settings of `mason` plugin
 --
 require('mason').setup()
 
----- settings of `lspzero` plugin
-----
---local lsp = require('lsp-zero')
+-- settings of `mason-lspconfig` plugin
 --
---lsp.preset('recommended')
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'gopls',
+    'pyright',
+    'denols',
+    'tsserver',
+  },
+  handlers = {
+    lspzero.default_setup,
+
+    -- custom configuration for `lua_ls` language server
+    lua_ls = lspconf.lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim', 'require' },
+          },
+        },
+      },
+    }),
+
+    -- custom configuration for `denols` language server
+    denols = lspconf.denols.setup({
+      init_options = {
+        enable = true,
+        lint = true,
+        unstable = true,
+      },
+      root_dir = lsputil.root_pattern('deps.ts'),
+    }),
+
+    -- custom configuration for `tsserver` language server
+    tsserver = lspconf.tsserver.setup({
+      init_options = {
+        preferences = {
+          quotePreference = 'single',
+          disableSuggestions = true,
+        },
+      },
+      single_file_support = false,
+      root_dir = lsputil.root_pattern('package.json'),
+    }),
+  },
+})
+
+-- settings of autocompletion plugins
 --
---lsp.set_preferences({
---  suggest_lsp_servers = true,
---})
---
---lsp.ensure_installed({
---  'gopls',
---  'pyright',
---  'denols',
---  'tsserver',
---})
---
----- settings of `lspconfig` plugin
-----
---local lspconfig = require('lspconfig')
---local lsputil = require('lspconfig.util')
---
---lspconfig.lua_ls.setup(
---  lsp.nvim_lua_ls({
---    settings = {
---      Lua = {
---        diagnostics = {
---          globals = { 'vim', 'require' },
---        },
---      },
---    },
---  })
---)
---
---lspconfig.denols.setup({
---  init_options = {
---    enable = true,
---    lint = true,
---    unstable = true,
---  },
---  root_dir = lsputil.root_pattern('deps.ts'),
---});
---
---lspconfig.tsserver.setup({
---  init_options = {
---    preferences = {
---      quotePreference = 'single',
---      disableSuggestions = true,
---    },
---  },
---  single_file_support = false,
---  root_dir = lsputil.root_pattern('package.json'),
---})
---
---lsp.on_attach(function(_, bufnr)
---  lsp.default_keymaps({ buffer = bufnr })
---
---  local options = { noremap = true, buffer = bufnr }
---
---  -- enable completion trigger by <C-x><C-o>
---  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---
---  vim.keymap.set('n', 'K', vim.lsp.buf.hover, options)
---  vim.keymap.set('n', '<Leader>K', vim.lsp.buf.signature_help, options)
---  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, options)
---  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, options)
---  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, options)
---  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, options)
---  vim.keymap.set('n', 'gr', vim.lsp.buf.references, options)
---  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, options)
---end)
---
---local cmp = require('cmp')
---local cmp_select = { behavior = cmp.SelectBehavior.Select }
---local cmp_mapping = lsp.defaults.cmp_mappings({
---  ['<C-K>'] = cmp.mapping.complete(),
---  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
---  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
---  ['<CR>'] = cmp.mapping.confirm({
---    behavior = cmp.ConfirmBehavior.Replace,
---    select = false,
---  }),
---})
---
---lsp.setup_nvim_cmp({
---  mapping = cmp_mapping
---})
---
---lsp.setup()
---
-vim.diagnostic.config({
-  virtual_text = true,
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+    -- Move up and down between completion menu items
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+
+    -- `Ctrl+e` to close completion menu
+    ['<C-e>'] = cmp.mapping.close(),
+
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+  })
 })
 
 -- settings of `copilot` plugin
@@ -353,12 +324,25 @@ vim.api.nvim_set_keymap(
 -- ---
 -- set up plugins' keyboard mappings
 --
+
+-- set up mappings for `telecope` plugin
+--
 local telescope_builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<Leader>tf', telescope_builtin.find_files, {})
 vim.keymap.set('n', '<Leader>tg', telescope_builtin.git_files, {})
 
-vim.keymap.set('n', '<Leader>gG', vim.cmd.Git)
+vim.keymap.set('n', '<Leader>gg', vim.cmd.Git)
 vim.keymap.set('n', '<Leader>gh', '<ESC>:noh<CR>')
+
+-- ---
+-- set up diagnostics
+--
+
+vim.diagnostic.config({
+  virtual_text = true,
+})
+
 
 -- ---
 -- set up neovide options
