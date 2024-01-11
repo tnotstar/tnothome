1b226f43fc5d362df94a7b0b0bc0fdd2d1b24d2a
Antonio Alvarado Hernández <tnotstar@gmail.com>
2024-01-11 17:21:30 +0100

    fix bad page size guess & add firstpage -F flag

--- bin/pdfcover.py
+++ bin/pdfcover.py
@@ -19,10 +19,10 @@ DEFAULT_IMAGE_FORMAT = r"pdf"
 DEFAULT_QUALITY_PERCENT = 100
 DEFAULT_DENSITY_POINTS = 96
 
-INCHES_PER_POINT = 1. / 72.
+INCHES_PER_POINT = 1./72.
 
 
-def add_cover_page(output_path: Path, image_path: Path, pdf_path: Path) -> None:
+def add_cover_page(output_path: Path, image_path: Path, input_path: Path, skip_firstpage: bool) -> None:
     """Add a cover page to given PDF file."""
 
     # create a temporary file to store the cover page
@@ -30,16 +30,17 @@ def add_cover_page(output_path: Path, image_path: Path, pdf_path: Path) -> None:
     os.close(cover_fd)
 
     # retrieve the image dimensions
-    pdf_reader = PdfReader(pdf_path)
-    width, height = get_most_frequent_dimensions(pdf_reader)
+    pdf_reader = PdfReader(input_path)
+    start_at = 1 if skip_firstpage else 0
+    width, height = get_most_frequent_dimensions(pdf_reader, start_at)
 
     # resize the image to fit the page
     with Image(filename=str(image_path)) as cover:
         cover.format = DEFAULT_IMAGE_FORMAT
         cover.compression_quality = DEFAULT_QUALITY_PERCENT
         cover.resolution = DEFAULT_DENSITY_POINTS
-        adj_width = math.ceil(DEFAULT_DENSITY_POINTS * INCHES_PER_POINT * float(width))
-        adj_height = math.ceil(DEFAULT_DENSITY_POINTS * INCHES_PER_POINT * float(height))
+        adj_width = math.ceil(DEFAULT_DENSITY_POINTS * INCHES_PER_POINT * width)
+        adj_height = math.ceil(DEFAULT_DENSITY_POINTS * INCHES_PER_POINT * height)
         resize_geometry=f"{adj_width}!x{adj_height}!"
         cover.transform(resize=resize_geometry)
         cover.save(filename=str(cover_fname))
@@ -47,43 +48,55 @@ def add_cover_page(output_path: Path, image_path: Path, pdf_path: Path) -> None:
     # merge the cover page with the original PDF file
     pdf_writer = PdfWriter(output_path, trailer=pdf_reader)
     pdf_writer.addpages(PdfReader(cover_fname).pages)
-    pdf_writer.addpages(pdf_reader.pages[1:])
+    pdf_writer.addpages(pdf_reader.pages[start_at:])
     pdf_writer.write()
 
     # remove the temporary cover page file
     os.remove(cover_fname)
 
 
-def get_most_frequent_dimensions(pdf_reader: PdfReader) -> tuple:
+def get_most_frequent_dimensions(pdf_reader: PdfReader, start_at: int) -> tuple:
     """Return the most frequent dimensions tuple in given PDFReader instance."""
-    dimensions = {}
-    pages = pdf_reader.pages
-    for pno, page in enumerate(pages, start=1):
-        mediabox = [ math.floor(float(v)) for v in page.MediaBox ]
-        key = (mediabox[2] - mediabox[0], mediabox[3] - mediabox[1])
 
+    dimensions = {}
+    for _, page in enumerate(pdf_reader.pages[start_at:]):
+        if page.CropBox:
+            box = [ float(v) for v in page.CropBox ]
+        elif page.MediaBox:
+            box = [ float(v) for v in page.MediaBox ]
+        else:
+            raise ValueError(f"Can't locate a valid crop or media box for page: {_}")
+
+        key= (box[2] - box[0], box[3] - box[1])
         if key in dimensions:
             dimensions[key] += 1
         else:
             dimensions[key] = 1
+
     return max(dimensions, key=lambda x: dimensions[x])
 
 
 def main():
+    """This is the program entry-point"""
+
     parser = ArgumentParser(description="add a cover image to give PDF file")
+    parser.add_argument("-F", "--skip-firstpage", action="store_true", default=False,
+        help="skip first page from the input file")
     parser.add_argument("-o", "--output-filename", required=True,
-        help="the output file name")
+        help="the output pdf file name")
     parser.add_argument("-i", "--image-filename", required=True,
         help="the cover image file name")
-    parser.add_argument("-p", "--pdf-filename", required=True,
-        help="the PDF file name")
+    parser.add_argument("-p", "--input-filename", required=True,
+        help="the input pdf file name")
     args = parser.parse_args()
 
     try:
         output_path = Path(args.output_filename)
         image_path = Path(args.image_filename)
-        pdf_path = Path(args.pdf_filename)
-        add_cover_page(output_path, image_path, pdf_path)
+        input_path = Path(args.input_filename)
+        skip_firstpage = args.skip_firstpage
+        add_cover_page(output_path, image_path, input_path, skip_firstpage)
+
     except Exception as ex:
         print("Oops: " + str(ex))
 
