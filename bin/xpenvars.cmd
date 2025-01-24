c2630ffa4cce0d2b54e0c4ae47d6947d52650410
Antonio Alvarado Hernández <tnotstar@gmail.com>
2025-01-24 22:55:45 +0100

    fix minor typo & add overwrite of output file

--- bin/xpenvars.cmd
+++ bin/xpenvars.cmd
@@ -1,9 +1,9 @@
 @rem Copyright 2024, Antonio Alvarado Hernández <tnotstar@gmail.com>
 @rem -*- coding: utf-8 -*-
 
-@for /f "tokens=1-3 delims=/" %%a in ('echo %date%') @do (
-    @reg export "HKEY_CURRENT_USER\Environment" user-envars_%%c%%b%%a.reg
-    @reg export "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" system-envars_%%c%%b%%a.reg
+@for /f "tokens=1-3 delims=/" %%a in ('echo %date%') do @(
+    @reg export "HKEY_CURRENT_USER\Environment" User-Envars_%%c%%b%%a.reg /y
+    @reg export "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" System-Envars_%%c%%b%%a.reg /y
 )
 @goto eof
 
