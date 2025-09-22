f5808eab64ee556f24f5f23e2ccc0b91f9e9ea50
Antonio Alvarado Hernández <tnotstar@gmail.com>
2025-09-22 10:59:16 +0200

    add `vscode` to `dotter` configuration

--- bin/up2goes.cmd
+++ bin/up2goes.cmd
@@ -3,20 +3,14 @@
 @setlocal enableextensions
 @setlocal disabledelayedexpansion
 
-@echo Info: Installing Go language server...
-@go install "golang.org/x/tools/gopls@latest"
-
 @echo Info: Installing Delve debugger...
 @go install "github.com/go-delve/delve/cmd/dlv@latest"
 
+@echo Info: Installing Go language server...
+@go install "golang.org/x/tools/gopls@latest"
+
 @echo Info: Installing Go Staticcheck linter...
 @go install "honnef.co/go/tools/cmd/staticcheck@latest"
-
-@echo Info: Installing Simple Project Scaffolder for Go...
-@go install "github.com/vg006/vgo@latest"
-
-@echo Info: Installing Go Implementation Stubs generator...
-@go install "github.com/josharian/impl@latest"
 
 @echo Info: Installing Go Tags modifier...
 @go install "github.com/fatih/gomodifytags@latest"
@@ -24,8 +18,11 @@
 @echo Info: Installing Go Tests generator...
 @go install "github.com/cweill/gotests/gotests@latest"
 
-@echo Info: Installing Go Play client...
-@go install "github.com/haya14busa/goplay/cmd/goplay@latest"
+@echo Info: Installing Simple Project Scaffolder for Go...
+@go install "github.com/vg006/vgo@latest"
+
+@echo Info: Installing Go Implementation Stubs generator...
+@go install "github.com/josharian/impl@latest"
 
 @echo Info: Installing Elvish shell...
 @go install "src.elv.sh/cmd/elvish@latest"
@@ -41,6 +38,9 @@
 
 @echo Info: Installing Note utility...
 @go install "github.com/armand-sauzay/note@latest"
+
+@echo Info: Installing Go Play client...
+@go install "github.com/haya14busa/goplay/cmd/goplay@latest"
 
 @goto :eof
 @endlocal
